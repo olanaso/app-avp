f9258a9c864f0c86fdd07424206facc7dd001085
Carlos Rafael <quipan@gmail.com>
2020-07-07 11:48:04 -0500

    Cambios para UI y y mejora de funcionalidad

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Login from './pages/Login/Login.f7.html';
 import Menu from './pages/Menu/Menu.f7.html';
 import ActividadesCampo from './pages/ActividadesCampo/ActividadesCampo.f7.html';
 import IngresoMTC from './pages/IngresoMTC/IngresoMTC.f7.html';
+import PanelLeft from './pages/panel-left.f7.html';
 
 
 /*Configuracion*/
@@ -28,6 +29,10 @@ export default [
   {
     path: '/registro-ingreso-salida-mtc/',
     component: IngresoMTC,
+  },
+  {
+    path: '/panel-left/',
+    component: PanelLeft,
   },
   {
     path: '(.*)',
