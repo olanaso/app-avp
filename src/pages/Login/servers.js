ed1099718fd136d90b7f609eafa2705ad8aac05b
Erick Escalante Olano <ericks.escalanteolano@gmail.com>
2020-07-28 09:24:08 -0500

    modificado eseo

---
+++ src/pages/Login/servers.js
@@ -0,0 +1,11 @@
+export const servers=[
+  {  ipServidor: '200.121.128.102',
+    puertoServidor: '4000',
+    tiempoGPS: '60'},
+  {  ipServidor: '200.121.128.102',
+    puertoServidor: '4000',
+    tiempoGPS: '60'},
+  {  ipServidor: '200.121.128.102',
+      puertoServidor: '3002',
+      tiempoGPS: '60'}
+];
